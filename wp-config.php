bfd84b3dae70a1dba52c20c2df19e7ec4c9f7cb9
Theodorus Yoga <theodorusyoga_ti09@mail.ugm.ac.id>
2015-11-16 00:11:29 +0700

    initial commit

---
+++ wp-config.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ * The base configuration for WordPress
+ *
+ * The wp-config.php creation script uses this file during the
+ * installation. You don't have to use the web site, you can
+ * copy this file to "wp-config.php" and fill in the values.
+ *
+ * This file contains the following configurations:
+ *
+ * * MySQL settings
+ * * Secret keys
+ * * Database table prefix
+ * * ABSPATH
+ *
+ * @link https://codex.wordpress.org/Editing_wp-config.php
+ *
+ * @package WordPress
+ */
+
+// ** MySQL settings - You can get this info from your web host ** //
+/** The name of the database for WordPress */
+define('DB_NAME', 'YuriAlamEksport');
+
+/** MySQL database username */
+define('DB_USER', 'root');
+
+/** MySQL database password */
+define('DB_PASSWORD', '');
+
+/** MySQL hostname */
+define('DB_HOST', 'localhost');
+
+/** Database Charset to use in creating database tables. */
+define('DB_CHARSET', 'utf8mb4');
+
+/** The Database Collate type. Don't change this if in doubt. */
+define('DB_COLLATE', '');
+
+/**#@+
+ * Authentication Unique Keys and Salts.
+ *
+ * Change these to different unique phrases!
+ * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+ * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
+ *
+ * @since 2.6.0
+ */
+define('AUTH_KEY',         'a6Z-z-(,abE2@([-h)IS.Y + -J1gmPc9<g<mgV +4+~zxP6%/I@/!84,i+/fL7k');
+define('SECURE_AUTH_KEY',  '0%CXDqHKf$S6G-W!gd8pm{X{A.j2-OneZp=n,bB_2O.l0~`mK-JCj#($-)>4k`O9');
+define('LOGGED_IN_KEY',    '@i5TB+S9sP8I+$s&0LzR-iG[.$7}-HN:S/q|olu7-7s2j+0 Rm|&SkHlhy 7P<bF');
+define('NONCE_KEY',        '&H4WR0WP ,JVn&/A`+OT0{gi@(/+WO|M. <aBX8NAsq8K*YZ[,|6qd HtP-YmG=C');
+define('AUTH_SALT',        'Du~z2]g&lg|Bv=RQnIeRV6#h=8$(}M.9^{0<;PPw(3E#/u]q]h-pd@Q)j#_6}!4I');
+define('SECURE_AUTH_SALT', 'S@5^C]2J+:qA&Osach?R|{lfKd&3.X?pz^l7~/PaDFaRd[-.l9_F$@1o|3(#<F$~');
+define('LOGGED_IN_SALT',   'u%irg3Ch/-W#ZFZuT~^V,D:e+XsQRW|*)9>hefF%-Qs= W||u~PbP.HS7mQjR6uu');
+define('NONCE_SALT',       'O4p$%6yFY+KF,;Zw@|#M70GwV7+knf5_T,RO^$D%FT|~L&LGvc=Fx9>f/t+)W|Hn');
+
+/**#@-*/
+
+/**
+ * WordPress Database Table prefix.
+ *
+ * You can have multiple installations in one database if you give each
+ * a unique prefix. Only numbers, letters, and underscores please!
+ */
+$table_prefix  = 'wp_';
+
+/**
+ * For developers: WordPress debugging mode.
+ *
+ * Change this to true to enable the display of notices during development.
+ * It is strongly recommended that plugin and theme developers use WP_DEBUG
+ * in their development environments.
+ *
+ * For information on other constants that can be used for debugging,
+ * visit the Codex.
+ *
+ * @link https://codex.wordpress.org/Debugging_in_WordPress
+ */
+define('WP_DEBUG', false);
+
+/* That's all, stop editing! Happy blogging. */
+
+/** Absolute path to the WordPress directory. */
+if ( !defined('ABSPATH') )
+	define('ABSPATH', dirname(__FILE__) . '/');
+
+/** Sets up WordPress vars and included files. */
+require_once(ABSPATH . 'wp-settings.php');
---
+++ wp-content/themes/twentythirteen/header.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * The Header template for our theme
+ *
+ * Displays all of the <head> section and everything up till <div id="main">
+ *
+ * @package WordPress
+ * @subpackage Twenty_Thirteen
+ * @since Twenty Thirteen 1.0
+ */
+?>
+<!DOCTYPE html>
+<!--[if IE 7]>
+<html class="ie ie7" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if IE 8]>
+<html class="ie ie8" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if !(IE 7) & !(IE 8)]><!-->
+<html <?php language_attributes(); ?>>
+<!--<![endif]-->
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>">
+<meta name="viewport" content="width=device-width">
+<title><?php wp_title( '|', true, 'right' ); ?></title>
+<link rel="profile" href="http://gmpg.org/xfn/11">
+<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+<link rel="stylesheet"
+	href="<?php echo esc_url(get_template_directory_uri()); ?>/css/bootstrap.min.css">
+<link rel="stylesheet"
+	href="<?php echo esc_url(get_template_directory_uri()); ?>/css/scrolling-nav.css">
+<!--[if lt IE 9]>
+	<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js"></script>
+	<![endif]-->
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+	<div id="page" class="hfeed site">
+		<header id="masthead" class="site-header" role="banner">
+		
+			<div class="container">
+				<div>
+					<div class="collapse navbar-collapse">
+						<ul class="nav navbar-nav navbar-right">
+							<!-- Hidden li included to remove active class from about link when scrolled up past about section -->
+							<li class="hidden"><a class="page-scroll" href="#page-top"></a></li>
+							<li><a class="page-scroll"><?php get_search_form(); ?></a></li>
+							<li><a class="page-scroll" href="#services">Services</a></li>
+							<li><a class="page-scroll" href="#product">Product</a></li>
+							<li><a class="page-scroll" href="#partnership">Partnership</a></li>
+							<li><a class="page-scroll" href="#whyus">Why Us</a></li>
+							<li><a class="page-scroll" href="#contact">Contact</a></li>
+							
+						</ul>
+					</div>
+				</div>
+			</div>
+			<div id="navbar" class="navbar nav">
+				<nav id="site-navigation" class="navigation main-navigation"
+					role="navigation">
+					<button class="menu-toggle"><?php _e( 'Menu', 'twentythirteen' ); ?></button>
+					<a class="screen-reader-text skip-link" href="#content"
+						title="<?php esc_attr_e( 'Skip to content', 'twentythirteen' ); ?>"><?php _e( 'Skip to content', 'twentythirteen' ); ?></a>
+					<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'nav-menu', 'menu_id' => 'primary-menu' ) ); ?>
+					
+				</nav>
+				<!-- #site-navigation -->
+			</div>
+			<!-- #navbar -->
+		</header>
+		<!-- #masthead -->
+
+		<div id="main" class="site-main">
