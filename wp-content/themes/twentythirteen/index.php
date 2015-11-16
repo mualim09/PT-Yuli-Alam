264f8c336617ff5bec9e61a5929f69648b4ac55a
Theodorus Yoga <theodorusyoga_ti09@mail.ugm.ac.id>
2015-11-17 00:12:03 +0700

    udah bisa image scroller yey!

--- wp-content/themes/twentythirteen/index.php
+++ wp-content/themes/twentythirteen/index.php
@@ -15,15 +15,30 @@
  */
 get_header ();
 ?>
-
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0-alpha1/jquery.min.js"></script>
+<script type="text/javascript" src="<?php echo esc_url(get_template_directory_uri()); ?>/js/autoscroller.js"></script>
 <div id="primary" class="content-area">
 	<div id="content" class="site-content" role="main">
 		<div class="subcontent">
-			<div class="pictures"></div>
+
 			<h1>
 				<span class="block">&nbsp;</span>PT. Yuri Alam Eksport
 			</h1>
-
+			<div class="pictures">
+				<div class="scroll">
+					<ul>
+						<li><img
+							src="<?php echo esc_url(get_template_directory_uri()); ?>/images/Pict11 copy.jpg"
+							width="600" height="280" /></li>
+						<li><img
+							src="<?php echo esc_url(get_template_directory_uri()); ?>/images/Pict21 copy.jpg"
+							width="600" height="280" /></li>
+						<li><img
+							src="<?php echo esc_url(get_template_directory_uri()); ?>/images/Pict22 copy.jpg"
+							width="600" height="280" /></li>
+					</ul>
+				</div>
+			</div>
 		</div>
 		<div class="subcontent2">
 			<h1>
